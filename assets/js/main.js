e538bb9878f8cbbe4835e4053393f4acfac5cc23
mueliwa <88253869+mueliwa@users.noreply.github.com>
2021-09-13 12:28:30 +0200

    Eliwa13/9

--- assets/js/main.js
+++ assets/js/main.js
@@ -214,6 +214,24 @@
     }
   });
 
+/**
+   * Products slider
+   */
+ new Swiper('.products-slider', {
+  speed: 600,
+  loop: true,
+  autoplay: {
+    delay: 2000,
+    disableOnInteraction: false
+  },
+  slidesPerView: 'auto',
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true
+  }
+});
+
   /**
    * Porfolio isotope and filter
    */
